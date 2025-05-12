d10719bfe132288491abdd0e9772af0740dec6a6
Diablotinet <121221500+Diablotinet@users.noreply.github.com>
2025-05-12 17:42:53 +0200

    Login + Register

--- backend/server.js
+++ backend/server.js
@@ -24,18 +24,6 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-// MySQL connection
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueTimeout: 0
-});
-
 // launch the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
